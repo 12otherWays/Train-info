201839bcc18b9eca258c74e084094c5f602eb041
Reetu raj <reeturajsinghaniya31@gmail.com>
2022-12-14 09:39:23 +0530

    react-webpack template ready

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index.bundle.js",
+  },
+  devServer: {
+    port: 3010,
+    watchContentBase: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
